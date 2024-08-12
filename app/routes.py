46cdc037e7a0c95a8ae8265fdf885c07bbd7d66b
Devrim Savas <137702797+devrimsavas@users.noreply.github.com>
2024-08-12 05:47:55 +0200

    added name

--- app/routes.py
+++ app/routes.py
@@ -15,20 +15,23 @@ def staff():
 
     return render_template('staff.html',title=title)
 
-@main.route('/newstaffrecord',methods=["POST"])
+@main.route('/newstaffrecord', methods=["POST"])
 def newstaffrecord():
-    name=request.form.get("name");
-    age=request.form.get("age");
-    gender=request.form.get("gender");
-    position=request.form.get("position");
+    name = request.form.get("name")
+    age = request.form.get("age")
+    gender = request.form.get("gender")
+    position = request.form.get("position")
 
-    person= {
-        "name":name,
-        "age":age,
-        "gender":gender,
-        "position":position
-    }   
+    if not name or not age or not gender or not position:
+        return jsonify({"error": "All fields are required"}), 400
 
-    
+    person = {
+        "name": name,
+        "age": age,
+        "gender": gender,
+        "position": position
+    }
+
+    print("Person added is ----- :", person)
 
     return jsonify(person)
